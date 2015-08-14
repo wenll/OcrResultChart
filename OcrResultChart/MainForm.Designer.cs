56545d9f37be5e3b466ac420becc37a19e0c00e9
wenlianlong <18866855581@139.com>
2015-08-14 16:59:27 +0800

    Add source code to the repo.To run this application, you need a database.(ADD LATER)

---
+++ OcrResultChart/MainForm.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace OcrResultChart
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.cbAttach = new System.Windows.Forms.CheckBox();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.cbBatch = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.cbUnocr = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(560, 302);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // cbAttach
+            // 
+            this.cbAttach.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cbAttach.AutoSize = true;
+            this.cbAttach.Location = new System.Drawing.Point(386, 334);
+            this.cbAttach.Name = "cbAttach";
+            this.cbAttach.Size = new System.Drawing.Size(72, 16);
+            this.cbAttach.TabIndex = 11;
+            this.cbAttach.Text = "包括附件";
+            this.cbAttach.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnNext.Location = new System.Drawing.Point(305, 328);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 10;
+            this.btnNext.Text = "下一组";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // cbBatch
+            // 
+            this.cbBatch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cbBatch.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbBatch.FormattingEnabled = true;
+            this.cbBatch.Location = new System.Drawing.Point(64, 329);
+            this.cbBatch.Name = "cbBatch";
+            this.cbBatch.Size = new System.Drawing.Size(91, 20);
+            this.cbBatch.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 334);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "批次号";
+            // 
+            // btnShow
+            // 
+            this.btnShow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnShow.Location = new System.Drawing.Point(172, 329);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(126, 23);
+            this.btnShow.TabIndex = 7;
+            this.btnShow.Text = "查看分布曲线";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // cbUnocr
+            // 
+            this.cbUnocr.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cbUnocr.AutoSize = true;
+            this.cbUnocr.Checked = true;
+            this.cbUnocr.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbUnocr.Location = new System.Drawing.Point(464, 333);
+            this.cbUnocr.Name = "cbUnocr";
+            this.cbUnocr.Size = new System.Drawing.Size(84, 16);
+            this.cbUnocr.TabIndex = 12;
+            this.cbUnocr.Text = "包括未识别";
+            this.cbUnocr.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 362);
+            this.Controls.Add(this.cbUnocr);
+            this.Controls.Add(this.cbAttach);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.cbBatch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.chart1);
+            this.Name = "MainForm";
+            this.Text = "识别数据统计";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.CheckBox cbAttach;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.ComboBox cbBatch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.CheckBox cbUnocr;
+    }
+}
---
+++ OcrResultChart/MainForm.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using MySql.Data.MySqlClient;
+
+namespace OcrResultChart
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            var tbBatch = GetTable("SELECT DISTINCT(BatchID) FROM billdetailbasicinfo");
+            this.cbBatch.DataSource = tbBatch;
+            this.cbBatch.ValueMember = "BatchID";
+            this.cbBatch.DisplayMember = "BatchID";
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            var list = GetList(cbBatch.Text);
+            double result = 0;
+            int cnt = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] != 0 && i != 0)
+                {
+                    result += list[i] / (double)i;
+                    cnt++;
+                }
+            }
+            result = result / cnt;
+            chart1.Series.Clear();
+            Series series = new Series("Real");
+            series.ChartType = SeriesChartType.Point;
+            for (int i = 0; i < list.Count; i++)
+            {
+                series.Points.AddXY(i, list[i]);
+            }
+            series.IsValueShownAsLabel = true;
+            chart1.Series.Add(series);
+            Series line = new Series("Average");
+            line.ChartType = SeriesChartType.Line;
+            line.Color = Color.Red;
+            line.Points.AddXY(0.0, 0.0);
+            line.Points.AddXY(list.Count, list.Count * result);
+            chart1.Series.Add(line);
+
+            Series line_std = new Series("1:1");
+            line_std.ChartType = SeriesChartType.Line;
+            line_std.Color = Color.Green;
+            line_std.Points.AddXY(0, 0);
+            line_std.Points.AddXY(list.Count, list.Count);
+            chart1.Series.Add(line_std);
+
+            this.chart1.ChartAreas[0].AxisX.Minimum = 0;
+            this.chart1.ChartAreas[0].AxisX.Maximum = list.Count;
+            this.chart1.ChartAreas[0].AxisX.Interval = 5;
+            this.chart1.ChartAreas[0].AxisY.Interval = 5;
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (cbBatch.SelectedIndex < cbBatch.Items.Count - 1)
+            {
+                this.cbBatch.SelectedIndex += 1;
+                this.btnShow_Click(null, null);
+            }
+        }
+        private List<int> GetList(string batchid)
+        {
+            List<int> list = new List<int>();
+            var dtBill = GetTable("select ID,SeriID,isattachment from billdetailbasicinfo where isback = 0 and batchid = " + batchid);
+            var dtSeri = GetTable("select ID from serialnumberlistdetail where isattach = 0 and batchid = " + batchid);
+            var seriList = new List<int>();
+            foreach (DataRow row in dtSeri.Rows)
+            {
+                seriList.Add(int.Parse(row["ID"].ToString()));
+            }
+            for (int i = 0; i < dtBill.Rows.Count; i++)
+            {
+                DataRow dr = dtBill.Rows[i];
+                if (dr["isattachment"].ToString().Equals("1"))
+                {
+                    if (cbAttach.Checked)
+                    {
+                        list.Add(0);
+                    }
+                }
+                else
+                {
+                    string seriID = dr["SeriID"].ToString();
+                    if (string.IsNullOrEmpty(seriID) || seriID.Equals("-1"))
+                    {
+                        if (cbUnocr.Checked)
+                        {
+                            list.Add(0);
+                        }
+                    }
+                    else
+                    {
+                        list.Add(seriList.FindIndex(s => s == int.Parse(seriID)));
+                    }
+                }
+            }
+            return list;
+        }
+
+        private DataTable GetTable(string sql)
+        {
+            using (MySqlConnection connection = new MySqlConnection("server=75.41.0.154;user=root;database=lzbankdatabase;port=3306;password=root;"))
+            {
+                DataSet ds = new DataSet();
+                connection.Open();
+                try
+                {
+                    MySqlDataAdapter da = new MySqlDataAdapter();
+                    MySqlCommand cmd = new MySqlCommand(sql, connection);
+                    cmd.CommandTimeout = 600;
+                    da.SelectCommand = cmd;
+                    da.Fill(ds, "ds");
+                }
+                catch
+                { }
+                connection.Clone();
+                return ds.Tables[0];
+            }
+        }
+
+    }
+}
